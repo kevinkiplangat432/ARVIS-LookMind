eb4cf301aec0085dbc3febd98f65915d907e0b6b
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-04-15 05:52:35 +0300

    my first Golang code

---
+++ learning-Go/helloworld.go
@@ -0,0 +1,8 @@
+package main
+import ("fmt")
+func main() {
+	fmt.Println("hello world!")
+}
+
+// comment
+// The fmt package (pronounced "fumpt") in Go is a core library used for formatted I/O (input/output). It allows developers to format text, print data to various destinations, and read user input from the console or other sources. 
