8029a94a92269b4b27033f24339676f90856fd0a
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-04-28 12:10:19 +0300

    chore: proxyv1 read,use and restore the body in the server also did a bit of logging

--- learn-proxy/proxyv1.go
+++ learn-proxy/proxyv1.go
@@ -3,13 +3,32 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("hello"))
+		// read the body 
+		bodyBytes, err := io.ReadAll(r.Body)
+		if err != nil{
+			http.Error(w, "can't read Body", http.StatusInternalServerError)
+			return
+		}
+		// close the body when done 
+		defer r.Body.Close()
+		// use the data from the body
+		log.Println(r.Method, r.URL.Path, "Body:", string(bodyBytes))
+
+		switch r.Method{
+		case http.MethodGet:
+			w.Write([]byte("Received!"))
+		}
+		// restore the Body  put a fresh one in there 
+		r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+		
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
