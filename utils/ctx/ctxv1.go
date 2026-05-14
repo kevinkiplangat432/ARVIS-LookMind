b72b3aa00201c5829ba34db6bf89960f54817c3f
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-05-14 06:11:31 +0300

    wrote up the key generator

--- utils/ctx/ctxv1.go
+++ utils/ctx/ctxv1.go
@@ -1,6 +1,11 @@
 package ctx
 
-import "context"
+import (
+	"context"
+	"crypto/rand"
+	"fmt"
+	"time"
+)
 
 const RequestID3 key2 = "RequestID"
 type key2 string
@@ -14,4 +19,15 @@ func SetRequestID3(ctx context.Context, Ctxid string) context.Context{
 func GetRequestIDv2(ctx context.Context) string {
 	requestid, _ := ctx.Value(RequestID3). (string)
 	return requestid
+}
+
+
+// generaing truly random keys 
+func generateId() string{
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil{
+		return "req-" + time.Now().Format("05.000")
+	}
+	return fmt.Sprintf("%x", b)
 }
