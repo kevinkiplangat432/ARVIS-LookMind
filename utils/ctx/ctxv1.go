bbd281ad1487ddef896513ec29ef575231b1762c
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-05-14 05:56:58 +0300

    implemented the getter and setter function from scratch as a refresher

--- utils/ctx/ctxv1.go
+++ utils/ctx/ctxv1.go
@@ -1,1 +1,17 @@
 package ctx
+
+import "context"
+
+const RequestID3 key2 = "RequestID"
+type key2 string
+
+// setter function 
+func SetRequestID3(ctx context.Context, Ctxid string) context.Context{
+	return context.WithValue(ctx, RequestID3, Ctxid)
+}
+
+// getter func 
+func GetRequestIDv2(ctx context.Context) string {
+	requestid, _ := ctx.Value(RequestID3). (string)
+	return requestid
+}
