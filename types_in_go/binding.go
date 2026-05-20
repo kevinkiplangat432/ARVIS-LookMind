abf709c8c430cf9d19025dcb73cb5bde77302fb3
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-05-20 18:16:05 +0300

    learn the  binding of struct

---
+++ types_in_go/binding.go
@@ -0,0 +1,29 @@
+package main
+// import (
+// 	"fmt"
+// )
+
+// type Userv2 struct{
+// 	ID int
+// 	Username string
+
+// }
+
+// // value Receiver 
+// func (u Userv2) Greet() string{
+// 	return "Hello, " + u.Username
+// }
+
+// // pointer receiver can modify the original struct 
+// func (u *Userv2) Deactivate() {
+// 	// modify the actual struct instace directly
+// 	u.Username = "deactivated"
+// }
+
+// func main() {
+// 	u := Userv2{ID: 101,
+// 	Username: "john_doe",
+// }
+
+// 	fmt.Println(u.Greet())
+// }
--- types_in_go/main.go
+++ types_in_go/main.go
@@ -3,12 +3,26 @@ package main
 import(
 	"fmt"
 )
+var count int       // 0
+var balance float64 // 0.0
+var active bool     // false
+var name string  
+
+// just because Go does not want me to set up stuff and not use them actually
+
+func print_stuff(){
+	fmt.Printf("count: %d\n", count)
+	fmt.Printf("balance: %.2f\n", balance)
+	fmt.Printf("active: %t\n", active)
+	fmt.Printf("name: '%s'\n", name)
+}
 
 type User struct {
 	ID int
 	Username string
 	IsActive bool
 }
+
 
 func main(){
 	// cteating a struct instance using a literal struct definition
@@ -19,5 +33,12 @@ func main(){
 	}
 	// Accessing fields using dot notation
 	fmt.Printf("user %d: %s (Active: %t)\n", u.ID, u.Username, u.IsActive)
+	print_stuff()
 }
 
+
+// (u User) is a value receiver
+
+func (u User) ProfileMessage() string {
+	return "User: " + u.Username
+}
