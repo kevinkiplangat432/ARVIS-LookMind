81167678dfd3d8f626e8a0535c91fc8e50f33287
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-05-22 05:33:43 +0300

    tried to buildabackend server

--- types_in_go/binding.go
+++ types_in_go/binding.go
@@ -1,24 +1,24 @@
 package main
-// import (
-// 	"fmt"
-// )
+import (
+	// "fmt"
+)
 
-// type Userv2 struct{
-// 	ID int
-// 	Username string
+type Userv2 struct{
+	ID int
+	Username string
 
-// }
+}
 
-// // value Receiver 
-// func (u Userv2) Greet() string{
-// 	return "Hello, " + u.Username
-// }
+// value Receiver 
+func (u Userv2) Greet() string{
+	return "Hello, " + u.Username
+}
 
-// // pointer receiver can modify the original struct 
-// func (u *Userv2) Deactivate() {
-// 	// modify the actual struct instace directly
-// 	u.Username = "deactivated"
-// }
+// pointer receiver can modify the original struct 
+func (u *Userv2) Deactivate() {
+	// modify the actual struct instace directly
+	u.Username = "deactivated"
+}
 
 // func main() {
 // 	u := Userv2{ID: 101,
