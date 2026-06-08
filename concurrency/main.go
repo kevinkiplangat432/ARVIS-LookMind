06e33342bc35a3d802a5acb44205d1061f21126a
kevin kiplangat <kiplangatkevin335@gmail.com>
2026-06-08 17:38:38 +0300

    learning fucking concurrenctry

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -12,13 +11,16 @@ type Email struct {
 	Address string
 }
 // oass a pointer so the wait group can modify the original counter
-func sendEmail(email Email, wg *sync.WaitGroup){
+// refactor v2 pass a channel (chan string ) into the function
+func sendEmail(email Email, resultChan chan string){
 
-	// defer tells go to run wg.Done() right before this function exits
-	defer wg.Done()
-	// simulate network latency for sending an email
 	time.Sleep(100 * time.Millisecond)
-	fmt.Printf("Newsletter send to %s\n",email.Address)
+	//create a success message string 
+	status := fmt.Sprintf("Newsletter send to %s\n",email.Address)
+
+	// shove the massage into the channel using the <- arrow
+
+	resultChan <- status
 
 }
 
@@ -26,7 +28,35 @@ func main() {
 	start := time.Now()
 
 	// a mock list of subscribers 
+	// buffered channel 
+	limitChan := make(chan int, 2)
 
+	totalJobs := 4 
+
+	fmt.Println( "starting rate-limited newsletter broadcast.....")
+
+	for i := 1; i <= totalJobs; i++ {
+		// shove a token into the channel buffer before starting the worker 
+		// if the waiting room is full it blocks and waits
+
+		limitChan <- i
+		fmt.Printf("[Slot] Job %d entered the waiting room\n", i)
+
+		go func(jobID int) {
+			// this anonymous function represents our email sender 
+			fmt.Printf("  [➔] Processing Email Job %d...\n", jobID)
+			time.Sleep(500 * time.Millisecond) // Simulated heavy work
+			fmt.Printf("  [✓] Finished Email Job %d\n", jobID)
+
+			// take the token out of the channel when done, freeing up a slot
+			<-limitChan
+
+		}(i)
+
+		// a quickie cheat to let the final background jobs print before exiting
+		time.Sleep(1500 * time.Millisecond)
+		fmt.Printf("Broadcast done! Total time: %v \n ", time.Since(start))
+	}
 	subscribers := []Email{
 		{ID: 1, Address: "alice@example.com"},
 		{ID: 2, Address: "bob@example.com"},
@@ -34,18 +64,24 @@ func main() {
 		{ID: 4, Address: "david@example.com"},
 
 	}
-	// initialize the wait group
-	var wg sync.WaitGroup
+	// create a channel that only holds "string" data types 
+	resultChan := make(chan string)
+
 
 	fmt.Println("Starting newsletter broadcast ...")
+
 	//native concurrent implementation (fire and forget)
 	for _, sub := range subscribers{
 		// increment the wait group counter for each email we plan to send
-		wg.Add(1)
-		go sendEmail(sub, &wg)
+		go sendEmail(sub, resultChan)
 	}
-	//block main here untill the counter goes down to 0
-	wg.Wait()
+
+	// pull data out of the channel 4 times 
+	for i := 0; i < 5; i++ {
+		// This line BLocks and wait untill a go routine shows sth
+		msg := <- resultChan
+		fmt.Println(msg)
+	}
 
 	fmt.Printf("Broadcast routine finished in %v\n", time.Since(start))
 
